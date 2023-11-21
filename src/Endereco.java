970c535ae57b12e47493a9719589d1726568cdf5
Barbara Barbabela <108891311+barbarabarbabela@users.noreply.github.com>
2023-11-21 15:36:12 -0300

    Update Endereco.java

--- src/Endereco.java
+++ src/Endereco.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public record Endereco() {
+public record Endereco(String cep, String logradouro,
+                       String complemento, String bairro,
+                       String localidade, String uf) {
+    @Override
+    public String toString() {
+        return "Endereco: " +
+                "\nCEP: " + cep +
+                "\nLogradouro: " + logradouro +
+                "\nComplemento: " + complemento +
+                "\nBairro: " + bairro +
+                "\nLocalidade: " + localidade +
+                "\nUF: " + uf ;
+    }
 }
